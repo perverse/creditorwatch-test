ccfb7e0ca8ebd0900afe627ae59424bc8a4de773
Ronnie Pyne <ronniepyne@gmail.com>
2019-11-11 19:57:23 +1100

    Fix up cli-server workings

--- public/index.php
+++ public/index.php
@@ -2,5 +2,16 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// accomodate php built in cli server
+if (php_sapi_name() == 'cli-server') {
+    $uri = urldecode(
+        parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+    );
+    
+    if ($uri !== '/' && file_exists(__DIR__ . '/' . $uri)) {
+        return false;
+    }
+}
+
 $bootstrap = new Kernel\Bootstrap;
 $bootstrap->run()->send();
